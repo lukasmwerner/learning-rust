c8b2f90c929349cca6d118b08a229cad12fe7fa6
Lukas Werner <lwerner@lshigh.org>
2022-03-15 14:28:25 -0700

    use a memoized redis cache

--- shakespeareswords/src/main.rs
+++ shakespeareswords/src/main.rs
@@ -1,3 +1,4 @@
+use redis::{Commands, RedisResult};
 use rocket::routes;
 use rocket_dyn_templates::Template;
 use serde::{Deserialize, Serialize};
@@ -45,6 +46,29 @@ async fn get_results(word: &str) -> Result<Vec<WordsResult>, Box<dyn std::error:
     Ok(results)
 }
 
+async fn memo_results(word: &str) -> Vec<WordsResult> {
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let mut con = client.get_connection().unwrap();
+
+    let result : RedisResult<String> = con.get(&word);
+    match result {
+        Ok(value) => {
+            let results: Vec<WordsResult> = serde_json::from_str(&value).unwrap();
+            return results;
+        }
+        Err(_) => {
+            let res = get_results(word).await;
+            if res.is_err() {
+                return Vec::new();
+            }
+            let res = res.unwrap();
+            let value = serde_json::to_string(&res).unwrap();
+            let _ = con.set::<String, String, String>(word.to_string(), value);
+            return res;
+        },
+    }
+}
+
 #[get("/")]
 fn index() -> Template {
     Template::render(
@@ -58,9 +82,10 @@ fn index() -> Template {
 
 #[get("/?<query>")]
 async fn search(query: &str) -> Template {
-    let results = get_results(query).await;
+    //let results = get_results(query).await;
+    let results = memo_results(query).await;
 
-    if results.is_err() {
+    /*if results.is_err() {
         return Template::render(
             "index",
             Context {
@@ -73,7 +98,7 @@ async fn search(query: &str) -> Template {
             },
         );
     }
-    let results = results.unwrap();
+    let results = results.unwrap();*/
 
     Template::render(
         "index",
